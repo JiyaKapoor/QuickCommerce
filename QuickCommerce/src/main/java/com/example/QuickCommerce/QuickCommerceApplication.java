7c57f34e677a8107a59e501c76553e04edf7c22f
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-20 16:04:07 +0530

    Initial commit

---
+++ QuickCommerce/src/main/java/com/example/QuickCommerce/QuickCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuickCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickCommerceApplication.class, args);
+	}
+
+}
---
+++ QuickCommerce/src/test/java/com/example/QuickCommerce/QuickCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuickCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
