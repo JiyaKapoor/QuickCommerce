17b04de14388b6c64e8d477fc0010ee3db2ee490
JiyaKapoor <jiyakapoor1409@gmail.com>
2026-06-24 21:33:09 +0530

    viewItems() method added to view products from nearby warehouses

--- QuickCommerce/src/main/java/com/example/QuickCommerce/Services/UserService.java
+++ QuickCommerce/src/main/java/com/example/QuickCommerce/Services/UserService.java
@@ -2,10 +2,12 @@ package com.example.QuickCommerce.Services;
 
 import com.example.QuickCommerce.Entity.Cart;
 import com.example.QuickCommerce.Entity.DarkHouse;
+import com.example.QuickCommerce.Entity.Product;
 import com.example.QuickCommerce.Entity.User;
 import com.example.QuickCommerce.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -24,11 +26,12 @@ public class UserService {
         user_cart.setCurrCartItems(map);
     }
     //we require a function to view all the products from the valid darkhouses
-    public void viewItems(long userId){
+    public List<Product> viewItems(long userId){
         User user=userRepo.findById(userId).orElseThrow();
         double user_x=user.getX();
         double user_y= user.getY();
         List<DarkHouse> validDarkHouses=darkHouseService.fetchNearestWarehouse(5,user_x,user_y);
-
+        List<Product> products=new ArrayList<>();
+        return products;
     }
 }
